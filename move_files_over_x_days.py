0772c039f283968955a9a4bdebf2bd7ea5c973d9
rumjack <stdev@openmailbox.org>
2015-06-28 09:29:43 +0200

    Create move_files_over_x_days.py

---
+++ move_files_over_x_days.py
@@ -0,0 +1,9 @@
+import shutil, sys, time, os								# Import the header files
+src = 'u:\\test'												# Set the source directory
+dst = 'c:\\test'												# Set the destination directory
+
+now = time.time()											# Get the current time
+for f in os.listdir(src):										# Loop through all the files in the source directory
+    if os.stat(f).st_mtime < now - 240 * 86400:	# Work out how old they are, if they are older than 240 days old
+        if os.path.isfile(f):									# Check it's a file
+            shutil.move(f, dst)								# Move the files
